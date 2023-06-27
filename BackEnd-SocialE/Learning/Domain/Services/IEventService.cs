eacff9973169c15a914276bbbc0fb95b66d0b4cd
AnthonyNunez500 <u202119276@upc.edu.pe>
2023-06-27 16:37:06 -0500

    EventResource.cs IEventService.cs y SaveEventResource.cs

---
+++ BackEnd-SocialE/Learning/Domain/Services/IEventService.cs
@@ -0,0 +1,12 @@
+﻿using BackEnd_SocialE.Learning.Domain.Models;
+using BackEnd_SocialE.Learning.Domain.Services.Communication;
+
+namespace BackEnd_SocialE.Learning.Domain.Services;
+
+public interface IEventService
+{
+    Task<IEnumerable<Event>> ListAsync();
+    Task<EventResponse> SaveAsync(Event _event);
+    Task<EventResponse> UpdateAsync(int id, Event _event);
+    Task<EventResponse> DeleteAsync(int id);
+}
---
+++ BackEnd-SocialE/Learning/Resources/EventResource.cs
@@ -0,0 +1,13 @@
+﻿namespace BackEnd_SocialE.Learning.Resources;
+
+public class EventResource
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public float Price { get; set; }
+    public string EventDate { get; set; }
+    public string StartTime { get; set; }
+    public string EndTime { get; set; }
+    //public User Manager { get; set; }
+}

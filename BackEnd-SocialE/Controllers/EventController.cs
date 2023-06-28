beceb63b33b9e16575a4b9140be8c8b807f1eaed
AnthonyNunez500 <u202119276@upc.edu.pe>
2023-06-28 10:10:50 -0500

    Controller del Evento y Extensions (StringExtensions.cs; ModelStateExtensions.cs; ModelBuilderExtensions.cs )

---
+++ BackEnd-SocialE/Controllers/EventController.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using BackEnd_SocialE.Learning.Domain.Models;
+using BackEnd_SocialE.Learning.Domain.Services;
+using BackEnd_SocialE.Learning.Resources;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackEnd_SocialE.Controllers;
+
+[ApiController]
+[Route("/api/v1/[controller]")]
+public class EventController {
+    private readonly IEventService _eventService;
+    private readonly IMapper _mapper;
+
+    public EventController(IEventService eventService, IMapper mapper) {
+        _eventService = eventService;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<EventResource>> GetAllAsync() {
+        var events = await _eventService.ListAsync();
+        var resources = _mapper.Map<IEnumerable<Event>, IEnumerable<EventResource>>(events);
+        return resources;
+    }
+}

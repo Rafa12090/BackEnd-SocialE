7bc90ffa8a6d58b4b8f21b51eb007f5ba03597fc
AnthonyNunez500 <u202119276@upc.edu.pe>
2023-06-29 12:48:37 -0500

    Segunda versión de Security. Aún falta modificar el modelo de Evento.cs,

--- BackEnd-SocialE/Program.cs
+++ BackEnd-SocialE/Program.cs
@@ -3,21 +3,59 @@ using BackEnd_SocialE.Learning.Domain.Services;
 using BackEnd_SocialE.Learning.Mapping;
 using BackEnd_SocialE.Learning.Persistence.Repositories;
 using BackEnd_SocialE.Learning.Services;
+using BackEnd_SocialE.Security.Authorization.Handlers.Implementations;
+using BackEnd_SocialE.Security.Authorization.Handlers.Interfaces;
+using BackEnd_SocialE.Security.Authorization.MiddleWare;
+using BackEnd_SocialE.Security.Authorization.Settings;
 using BackEnd_SocialE.Security.Domain.Repositories;
 using BackEnd_SocialE.Security.Persistence.Repositories;
 using BackEnd_SocialE.Security.Services;
 using BackEnd_SocialE.Shared.Persistence.Contexts;
 using BackEnd_SocialE.Shared.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Add CORS
+builder.Services.AddCors();
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options => {
+    options.SwaggerDoc("v1", new OpenApiInfo {
+        Version = "v1",
+        Title = "BackEnd SocialE",
+        Description = "BackEnd SocialE, Restful Api",
+        TermsOfService = new Uri("https://social-e.com/tos"),
+        Contact = new OpenApiContact
+        {
+            Name = "OnService",
+            Url = new Uri("https://onservice.studio")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "SocialE Resources License",
+            Url = new Uri("https://social-e.com/license")
+        }
+    });
+    options.EnableAnnotations();
+    options.AddSecurityDefinition("bearerAuth", new OpenApiSecurityScheme {
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        Description = "JWT Authorization header using the Bearer scheme."
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        { new OpenApiSecurityScheme {
+                Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "bearerAuth" }
+            }, Array.Empty<string>()
+        } 
+    });
+});
 
 // Add Database Connection
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
@@ -30,14 +68,18 @@ builder.Services.AddDbContext<AppDbContext>(
 
 // Add lowercase routes
 builder.Services.AddRouting(options => options.LowercaseUrls = true);
-
+// AppSettings Configuration
+builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
 // Security Injection Configuration
 builder.Services.AddScoped<IEventRepository, EventRepository>();
 builder.Services.AddScoped<IEventService, EventService>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-
+// Security Injection Configuration
+builder.Services.AddScoped<IJwtHandler, JwtHandler>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
 // AutoMapper Configuration
 builder.Services.AddAutoMapper(
     typeof(ModelToResourceProfile),
@@ -57,8 +99,21 @@ using (var context = scope.ServiceProvider.GetService<AppDbContext>()) {
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options => {
+        options.SwaggerEndpoint("v1/swagger.json", "v1");
+        options.RoutePrefix = "swagger";
+    });
 }
+// Configure CORS
+app.UseCors(x => x
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
+
+// Configure Error Handler Middleware
+app.UseMiddleware<ErrorHandlerMiddleware>();
+// Configure JWT Handling
+app.UseMiddleware<JwtMiddleware>();
 
 app.UseHttpsRedirection();
 
---
+++ BackEnd-SocialE/Security/Authorization/MiddleWare/JwtMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using BackEnd_SocialE.Security.Authorization.Handlers.Interfaces;
+using BackEnd_SocialE.Security.Authorization.Settings;
+using BackEnd_SocialE.Security.Services;
+
+namespace BackEnd_SocialE.Security.Authorization.MiddleWare;
+
+public class JwtMiddleware {
+    private readonly RequestDelegate _next;
+    private readonly AppSettings _appSettings;
+
+    public JwtMiddleware(RequestDelegate next, AppSettings appSettings) {
+        _next = next;
+        _appSettings = appSettings;
+    }
+    
+    public async Task Invoke(HttpContext context, IUserService userService, IJwtHandler handler) {
+        var token = context.Request
+            .Headers["Authorization"].FirstOrDefault()?.Split("").Last();
+        var userId = handler.ValidateToken(token);
+        if (userId != null) {
+            context.Items["User"] = await userService.GetByIdAsync(userId.Value);
+        }
+        await _next(context);
+    }
+}
